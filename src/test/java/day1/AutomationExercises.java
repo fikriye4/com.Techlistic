1ed205209c909ec4d6bde6bd606a9bb77359983f
jessSchnere <iclalsener233@gmail.com>
2022-09-20 16:12:58 +0300

    Initial commit

---
+++ src/test/java/day1/AutomationExercises.java
@@ -0,0 +1,51 @@
+package day1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+import static org.junit.Assert.assertEquals;
+
+public class AutomationExercises {
+    //1. Launch browser
+    //2. Navigate to url 'http://automationexercise.com'
+    //3. Verify that home page is visible successfully
+    //4. Click on 'Test Cases' button
+    //5. Verify user is navigated to test cases page successfully
+    WebDriver driver;
+
+   @Before
+    public void setUp(){
+       WebDriverManager.chromedriver().setup();
+       driver = new ChromeDriver();
+       driver.manage().window().maximize();
+       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+   }
+   @After
+    public void tearDown(){
+    // driver.quit();
+   }
+   @Test
+    public void test07(){
+       driver.get("http://automationexercise.com");
+       boolean homePage=driver.findElement(By.xpath("//i[@class='fa fa-home']")).isDisplayed();
+
+       System.out.println(homePage?"Passed":"Failed");
+       driver.findElement(By.xpath("(//i[@class='fa fa-list'])[1]")).click();
+       String actualUrl=driver.getCurrentUrl();
+      String expectedUrl="https://automationexercise.com/test_cases";
+      assertEquals("faild",expectedUrl, actualUrl);
+
+
+   }
+
+
+    }
+
+
